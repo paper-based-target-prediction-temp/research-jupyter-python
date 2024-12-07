edd9ae2a00b0dd484146093cb5ea5d9dfc28c113
changwoo <124979889+okchangwoo@users.noreply.github.com>
2024-12-08 01:39:44 +0900

    mongodb 설정 단순화

--- src/core/config.py
+++ src/core/config.py
@@ -6,11 +6,6 @@ class Settings(BaseSettings):
     # MongoDB Settings
     MONGO_DB_URI: str = Field(..., description="MongoDB URI")
     MONGO_DB_NAME: str = Field(..., description="MongoDB DB Name")
-    MAX_CONNECTIONS_COUNT: int = Field(default=10, description="Max Connections Count")
-    MIN_CONNECTIONS_COUNT: int = Field(default=5, description="Min Connections Count")
-    SERVER_SELECTION_TIMEOUT_MS: int = Field(
-        default=20000, description="Server Selection Timeout (ms)"
-    )
 
     # Pubmed API Settings
     PUBMED_API_KEY: str = Field(..., description="Pubmed API Key")
